f45d8b5c880ddc0c4e3df100a1b6d9b28b3285c6
afzal <afzal.siddique99@gmail.com>
2021-08-18 19:28:02 +0600

    command pattern

---
+++ src/patterns/command/LightCommand.ts
@@ -0,0 +1,157 @@
+// @ts-ignore
+export interface Command {
+
+    execute(): string
+
+}
+
+
+export class Light {
+
+    public on(): string {
+        return 'on'
+    }
+
+    public off(): string {
+        return 'off'
+    }
+}
+var value:number=0
+export class RedLight {
+
+    public on(): string {
+        return `red${value}`
+    }
+
+    public off(): string {
+        return "off"
+    }
+
+    public increaseLuminosity(): string {
+        value++;
+        return `red${value}`
+    }
+
+    public decreaseLuminosity(): string {
+        value--;
+        return `red${value}`
+
+    }
+}
+
+export class LightOnCommand implements Command {
+
+    private _light: Light
+
+    constructor(_light: Light) {
+        this._light = _light;
+    }
+
+    execute(): string {
+        return this._light.on();
+    }
+
+
+}
+
+export class LightOffCommand implements Command {
+    private _light: Light
+
+    constructor(_light: Light) {
+        this._light = _light
+    }
+
+    execute(): string {
+        return this._light.off()
+    }
+
+}
+
+
+export class RedLightOn implements Command {
+    light: RedLight;
+
+    constructor(light: RedLight) {
+        this.light = light;
+    }
+
+    execute(): string {
+        return this.light.on()
+    }
+
+}
+
+export class RedLightOff implements Command {
+    light: RedLight;
+
+    constructor(light: RedLight) {
+        this.light = light;
+    }
+
+    execute(): string {
+        return this.light.off()
+    }
+
+}
+
+export class RedLightIncreaseLuminosity implements Command {
+    light: RedLight;
+
+    constructor(light: RedLight) {
+        this.light = light;
+    }
+
+    execute(): string {
+        return this.light.increaseLuminosity()
+    }
+
+}
+
+export class RedLightDecreaseLuminosity implements Command {
+    light: RedLight;
+
+    constructor(light: RedLight) {
+        this.light = light;
+    }
+
+    execute(): string {
+        return this.light.decreaseLuminosity()
+    }
+
+}
+
+
+export class RemoteController {
+    onCommand!: Command;
+
+    constructor() {
+    }
+
+    setCommand(onCommand: Command) {
+        this.onCommand = onCommand;
+    }
+
+    executeCommand() {
+        return this.onCommand.execute()
+    }
+
+
+}
+
+/*
+const light = new Light();
+const lightOnCommand = new LightOnCommand(light);
+const remoteController = new RemoteController();
+const lightOffCommand = new LightOffCommand(light)
+const newLight = new RedLight()
+const newLightCommand = new RedLightOn(newLight)
+const lightIncreased=new RedLightIncreaseLuminosity(newLight)
+remoteController.setCommand(lightOnCommand)
+remoteController.executeCommand()
+remoteController.setCommand(lightOffCommand)
+remoteController.executeCommand()
+remoteController.setCommand(newLightCommand)
+remoteController.executeCommand()
+remoteController.setCommand(lightIncreased)
+remoteController.executeCommand()
+*/
