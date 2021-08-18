4cbf7e8e59a13b17f1fc596597e6fa89695cd61f
afzal <afzal.siddique99@gmail.com>
2021-08-18 22:25:27 +0600

    command pattern

--- src/__tests__/command-pattern-light.test.ts
+++ src/__tests__/command-pattern-light.test.ts
@@ -1,19 +1,43 @@
 import {command_on_light} from "../pages/hello-command/implementCommand"
-import {RemoteController,LightYellow,LightYellowOn,LightYellowOff} from "../patterns/command/LightCommand"
+import {
+    RemoteController,
+    LightYellow,
+    LightYellowOn,
+    LightYellowOff,
+    LightRed,
+    LightRedOn, LightRedOff
+} from "../patterns/command/LightCommand"
 
 const remoteController = new RemoteController();
 describe("Command Pattern", () => {
     test("Yellow Light on",()=>{
-        let expectation = command_on_light(new LightYellowOn(new LightYellow()))
+        let expected = command_on_light(new LightYellowOn(new LightYellow()))
         remoteController.setCommand(new LightYellowOn(new LightYellow))
-        let reality=remoteController.executeCommand()
-        expect(expectation).toEqual(reality)
+        let actual=remoteController.executeCommand()
+        expect(expected).toEqual(actual)
     });
     test("Yellow Light off",()=>{
-        let expectation = command_on_light(new LightYellowOn(new LightYellow()))
-        remoteController.setCommand(new LightYellowOn(new LightYellow()))
-        let reality=remoteController.executeCommand()
-        expect(expectation).toEqual(reality)
-
+        let expected = command_on_light(new LightYellowOff(new LightYellow()))
+        remoteController.setCommand(new LightYellowOff(new LightYellow()))
+        let actual=remoteController.executeCommand()
+        expect(expected).toEqual(actual)
     })
+    test("Red Light off",()=>{
+        let expected = command_on_light(new LightRedOff(new LightRed()))
+        remoteController.setCommand(new LightRedOff(new LightRed()))
+        let actual=remoteController.executeCommand()
+        expect(expected).toEqual(actual)
+    })
+    test("Red Light on",()=>{
+        let expected = command_on_light(new LightRedOn(new LightRed()))
+        remoteController.setCommand(new LightRedOn(new LightRed))
+        let actual=remoteController.executeCommand()
+        expect(expected).toEqual(actual)
+    });
+    test("Red Light on",()=>{
+        let expected = command_on_light(new LightRedOn(new LightRed()))
+        remoteController.setCommand(new LightRedOn(new LightRed))
+        let actual=remoteController.executeCommand()
+        expect(expected).toEqual(actual)
+    });
 })
